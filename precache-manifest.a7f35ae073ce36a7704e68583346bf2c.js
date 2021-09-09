e5d714f03c9cbcf31559ab81a657b4d15973ff40
Saiful Islam <saifulislam@Saifuls-MacBook-Pro.local>
2021-09-10 00:51:06 +1000

    Updates

---
+++ precache-manifest.a7f35ae073ce36a7704e68583346bf2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac815659c62db02320fce1ea49f0b3a",
+    "url": "/flex-video-chat-client/index.html"
+  },
+  {
+    "revision": "1a2b966ea01b51144791",
+    "url": "/flex-video-chat-client/static/css/main.75b17f3f.chunk.css"
+  },
+  {
+    "revision": "35ee27ed528bbdddf009",
+    "url": "/flex-video-chat-client/static/js/2.43d9f9fa.chunk.js"
+  },
+  {
+    "revision": "1a2b966ea01b51144791",
+    "url": "/flex-video-chat-client/static/js/main.5a87d036.chunk.js"
+  },
+  {
+    "revision": "eadbe8ba60f098ecb837",
+    "url": "/flex-video-chat-client/static/js/runtime~main.ede027b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flex-video-chat-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
