8b7742561ea453f05ff777736de858b6c90aa640
Saiful Islam <saifulislam@Saifuls-MacBook-Pro.local>
2021-09-16 02:09:21 +1000

    many changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import VideoComp from './components/VideoComp';
+import ExampleComp from './components/example';
 
 function App() {
   return (
     <div className="App">
       <VideoComp />
+      {/* <ExampleComp /> */}
     </div>
   );
 }
